5e2a125cc356193eb065130985c7d139ef7a54d8
Joaquim Merlo <joaquimmerlo1@gmail.com>
2022-09-28 21:59:56 -0400

    last change

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { CancionesApp } from './CancionesApp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CancionesApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
