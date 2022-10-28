da04f830512273b50582585ecba524306b410124
Joaquim Merlo <joaquimmerlo1@gmail.com>
2022-10-27 21:52:45 -0400

    fixeddd

--- src/CancionesApp.jsx
+++ src/CancionesApp.jsx
@@ -1,11 +1,43 @@
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material'
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
-import { AppTheme } from './theme/AppTheme'
+
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+const purpleTheme = createTheme({
+  components: {
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          height: 90
+        }
+      }
+    }
+  },
+  palette: {
+    primary: {
+      main: '#1b003a'
+    },
+    secondary: {
+      main: '#56008c'
+    },
+    error: {
+      main: red.A400
+    },
+    action: {
+      main: '#fff'
+    }
+  }
+})
 
 export const CancionesApp = () => {
   return (
-    <AppTheme>
+    <ThemeProvider theme={purpleTheme}>
+      <CssBaseline />
+
       <AppRouter />
-    </AppTheme>
+    </ThemeProvider>
   )
 }
