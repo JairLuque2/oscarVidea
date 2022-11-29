cc3812d915fe902b754d105a8aa82d9519ca4716
JairLuque2 <70919714+JairLuque2@users.noreply.github.com>
2022-11-29 15:34:11 -0400

    mod

--- src/canciones/data/canciones.js
+++ src/canciones/data/canciones.js
@@ -18,101 +18,13 @@ export const canciones = [
     {
         id: "29",
         ritmo: "preciosaFlor",
-        title: "Aflicción",
-        linkCantado: "https://www.youtube.com/embed/brL-Kw1735Y",
+        title: "Preciosa Flor",
+        linkCantado: "https://www.youtube.com/embed/-_DfP8Qw_UQ",
         linkInstrumental: "",
         letra: ``
     },
     {
         id: "28",
-        ritmo: "preciosaFlor",
-        title: "Amor Juvenil",
-        linkCantado: "https://www.youtube.com/embed/PG9KmNBtqCo",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "27",
-        ritmo: "preciosaFlor",
-        title: "Sueños de Amor",
-        linkCantado: "https://www.youtube.com/embed/fwrN1fwybCc",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "26",
-        ritmo: "preciosaFlor",
-        title: "Por Tu Abandono",
-        linkCantado: "https://www.youtube.com/embed/vKdLWLq8HpQ",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "25",
-        ritmo: "preciosaFlor",
-        title: "Tu Perfume",
-        linkCantado: "https://www.youtube.com/embed/j-JughJmFSo",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "24",
-        ritmo: "preciosaFlor",
-        title: "Flor Cautiva",
-        linkCantado: "https://www.youtube.com/embed/1OuntsSkino",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "23",
-        ritmo: "preciosaFlor",
-        title: "Amor Imposible",
-        linkCantado: "https://www.youtube.com/embed/-1d7JX_6wyw",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "22",
-        ritmo: "preciosaFlor",
-        title: "Nunca te Olvidaré",
-        linkCantado: "https://www.youtube.com/embed/zNzIRPXo_FE",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "21",
-        ritmo: "preciosaFlor",
-        title: "Ofélia",
-        linkCantado: "https://www.youtube.com/embed/AcjlbF-cihI",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "20",
-        ritmo: "preciosaFlor",
-        title: "Por tu Traición",
-        linkCantado: "https://www.youtube.com/embed/ewZy_9kVHl4",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "19",
-        ritmo: "preciosaFlor",
-        title: "Orgullosa Rosa",
-        linkCantado: "https://www.youtube.com/embed/SK2FDd0fwok",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "18",
-        ritmo: "preciosaFlor",
-        title: "Noches de Nostalgia",
-        linkCantado: "https://www.youtube.com/embed/Lu9UOyGwhNE",
-        linkInstrumental: "",
-        letra: ``
-    },
-    {
-        id: "17",
         ritmo: "preciosaFlor",
         title: "Blanca Azucena",
         linkCantado: "https://www.youtube.com/embed/BJvHkvlGK3g",
@@ -120,94 +32,100 @@ export const canciones = [
         letra: ``
     },
     {
+        id: "27",
+        ritmo: "preciosaFlor",
+        title: "Noches de Nostalgia",
+        linkCantado: "https://www.youtube.com/embed/Lu9UOyGwhNE",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "26",
+        ritmo: "preciosaFlor",
+        title: "Orgullosa Rosa",
+        linkCantado: "https://www.youtube.com/embed/SK2FDd0fwok",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "25",
+        ritmo: "preciosaFlor",
+        title: "Por tu Traición",
+        linkCantado: "https://www.youtube.com/embed/ewZy_9kVHl4",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "24",
+        ritmo: "preciosaFlor",
+        title: "Ofélia",
+        linkCantado: "https://www.youtube.com/embed/AcjlbF-cihI",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "23",
+        ritmo: "preciosaFlor",
+        title: "Nunca te Olvidaré",
+        linkCantado: "https://www.youtube.com/embed/zNzIRPXo_FE",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "22",
+        ritmo: "preciosaFlor",
+        title: "Amor Imposible",
+        linkCantado: "https://www.youtube.com/embed/-1d7JX_6wyw",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "21",
+        ritmo: "preciosaFlor",
+        title: "Flor Cautiva",
+        linkCantado: "https://www.youtube.com/embed/1OuntsSkino",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "20",
+        ritmo: "preciosaFlor",
+        title: "Tu Perfume",
+        linkCantado: "https://www.youtube.com/embed/j-JughJmFSo",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "19",
+        ritmo: "preciosaFlor",
+        title: "Por Tu Abandono",
+        linkCantado: "https://www.youtube.com/embed/vKdLWLq8HpQ",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "18",
+        ritmo: "preciosaFlor",
+        title: "Sueños de Amor",
+        linkCantado: "https://www.youtube.com/embed/fwrN1fwybCc",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
+        id: "17",
+        ritmo: "preciosaFlor",
+        title: "Amor Juvenil",
+        linkCantado: "https://www.youtube.com/embed/PG9KmNBtqCo",
+        linkInstrumental: "",
+        letra: ``
+    },
+    {
         id: "16",
         ritmo: "preciosaFlor",
-        title: "Preciosa Flor",
-        linkCantado: "https://www.youtube.com/embed/-_DfP8Qw_UQ",
+        title: "Aflicción",
+        linkCantado: "https://www.youtube.com/embed/brL-Kw1735Y",
         linkInstrumental: "",
-        letra: `PRECIOSA FLOR DE TOMINA (cueca)	Letra y Música: OSCAR VIDEA
-	
-	
- 
-
-PRECIOSA FLOR DE MI LINDO VALLE
-
-ROMÁNTICA CHUQUISAQUEÑA
-
-HIJA DEL BELLO JARDÍN FLORIDO
-
-DE CONTAGIOSA SONRISA ERES TU
-
-CHOLITA DE BELLOS ATRIBUTOS
-			
-LA PREFERIDA DE MI CORAZON 
-
-
-SENCILLA MUJER DE MI PROVINCIA
-	
-CARITA ALEGRE Y RISUEÑA 
-
-DE  FACCIONES BONITAS LA DUEÑA
-
-AL VERTE ME INVENTO UN PIROPO
-
-ES QUE ESE TU PEINADO CON JOPO
-		
-ALIENTA TODAS MIS EMOCIONES
-
-
-BELLA MUJER DE PROVINCIA TOMINA
-
-POR  CONQUISTARTE CUANTO DARIA 
-
-MI CASA HE DEJADO POR QUERERTE
-
-ESTE MI CORAZON QUE TE MIMA
-
-AUN ESPERA ME DES LA RESPUESTA
-
-PORQUE MI ALMA IMPACIENTE ESTA
-
-	
-
-MI ENAMORADA CHUQUISAQUEÑA
-
-DE OJOS CLAROS QUE NO OLVIDARE
-
-CUANDO  A TI YO TE HE CONOCIDO
-
-A ORILLAS DEL RIO PILCOMAYO
-
-DE TI POR SIEMPRE PRENDADO ESTOY
-
-PRONTO ESPERO VOLVERTE A VER
-
-
-MUJER TOMINA TAN CONSENTIDA
-
-ESPLÉNDIDA CHOLITA  DE ENSUEÑO
-
-EN  MI VIDA ERES FLOR PRECIOSA
-
-QUIERO UN FUTURO UNIDO A TI
-
-ERES LA REINA DE MI CORAZON
-
-SOLO ESPERO QUE TU TE DECIDAS
-
-
-BELLA MUJER DE PROVINCIA TOMINA
-
-POR  CONQUISTARTE CUANTO DARIA 
-
-MI CASA HE DEJADO POR QUERERTE
-
-ESTE MI CORAZON QUE TE MIMA
-
-AUN ESPERA ME DES LA RESPUESTA
-
-PORQUE MI ALMA IMPACIENTE ESTA
-`
+        letra: ``
     },
     {
         id: "15",
