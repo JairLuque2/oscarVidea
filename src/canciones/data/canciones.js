406446b7c1e42baf111f26585e5b672accd9c9e7
JairLuque2 <70919714+JairLuque2@users.noreply.github.com>
2022-11-29 12:30:26 -0400

    ult canciones

--- src/canciones/data/canciones.js
+++ src/canciones/data/canciones.js
@@ -16,6 +16,30 @@
 
 export const canciones = [
     {
+        id: "6",
+        ritmo: "bolivianitaDulceColor",
+        title: "Orgullosa cholita",
+        linkCantado: "https://www.youtube.com/embed/zIJH0-cfpkU",
+        linkInstrumental: "https://www.youtube.com/embed/__Nx_F2QLCU",
+        letra: ``
+    },
+    {
+        id: "5",
+        ritmo: "bolivianitaDulceColor",
+        title: "Rosita",
+        linkCantado: "https://www.youtube.com/embed/Vl8ivBbAhgM",
+        linkInstrumental: "https://www.youtube.com/embed/3841xAXYdjg",
+        letra: ``
+    },
+    {
+        id: "4",
+        ritmo: "bolivianitaDulceColor",
+        title: "Tarateñita",
+        linkCantado: "https://www.youtube.com/embed/Pet2V36Icwg",
+        linkInstrumental: "https://www.youtube.com/embed/Cw73wk0_vuw",
+        letra: ``
+    },
+    {
         id: "3",
         ritmo: "bolivianitaDulceColor",
         title: "Te extraño bolivia",
